9e5023a314e0c1a26101bd71e796c9b1a6f62faf
uni2tw <uni2tw@gmail.com>
2018-08-29 18:47:05 +0800

    改成多緒，可同時連接多個smtp client

---
+++ nDumbster/SmtpMockServer/SimpleSmtpClicent.cs
@@ -0,0 +1,115 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+
+#endregion
+#region copyright
+/*
+ * nDumbster - a dummy SMTP server
+ * Copyright 2005 Martin Woodward
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#endregion // copyright
+
+namespace nDumbster.SmtpMockServer
+{
+    public class SimpleSmtpClicent
+    {
+        Socket _socket;
+        SimpleSmtpServer _server;
+        public SimpleSmtpClicent(SimpleSmtpServer server, Socket socket)
+        {
+            this._socket = socket;
+            this._server = server;
+        }
+
+        public void HandleSmtpTransaction()
+		{
+            // Get the input and output streams
+            NetworkStream networkStream = new NetworkStream(_socket);
+            StreamReader input = new StreamReader(networkStream);
+            StreamWriter output = new StreamWriter(networkStream);
+			// Initialize the state machine
+			SmtpState smtpState = SmtpState.CONNECT;
+			SmtpRequest smtpRequest = new SmtpRequest(SmtpActionType.CONNECT, String.Empty, smtpState);
+
+			// Execute the connection request
+			SmtpResponse smtpResponse = smtpRequest.Execute();
+
+			// Send initial response
+			SendResponse(output, smtpResponse);
+			smtpState = smtpResponse.NextState;
+
+            List<SmtpMessage> msgList = new List<SmtpMessage>();
+			SmtpMessage msg = new SmtpMessage();            
+			while (smtpState != SmtpState.CONNECT)
+			{
+				string line = input.ReadLine();
+
+				if (line == null)
+				{
+					break;
+				}
+
+				// Create request from client input and current state
+				SmtpRequest request = SmtpRequest.CreateRequest(line, smtpState);
+				// Execute request and create response object
+				SmtpResponse response = request.Execute();
+				// Move to next internal state
+				smtpState = response.NextState;
+				// Send reponse to client
+				SendResponse(output, response);
+
+				// Store input in message
+				msg.Store(response, request.Params);
+
+				// If message reception is complete save it
+				if (smtpState == SmtpState.QUIT)
+				{
+					msgList.Add(msg);
+
+                    MailMessageWrapper mmw = MailMessageWrapper.ConvertFrom(msg);
+                    this._server.TriggerMessageReceived(mmw);                        
+				    
+				    msg = new SmtpMessage();
+				}
+			    if (request.GetAction()== SmtpActionType.EHLO)
+			    {
+			        msg.HostName = request.Params;
+			    }
+			}
+
+            _socket.Close();
+            _server.TriggerClientDisconnected();
+            SimpleSmtpServer.ReceivedEmail.AddRange(msgList);
+		}
+
+		/// <summary>
+		/// Send response to client.
+		/// </summary>
+		/// <param name="output">socket output stream</param>
+		/// <param name="smtpResponse">Response to send</param>
+		private void SendResponse(StreamWriter output, SmtpResponse smtpResponse)
+		{
+			if (smtpResponse.Code > 0)
+			{
+				output.WriteLine(smtpResponse.Code + " " + smtpResponse.Message);
+				output.Flush();
+			}
+		}
+    }
+}
--- nDumbster/SmtpMockServer/SimpleSmtpServer.cs
+++ nDumbster/SmtpMockServer/SimpleSmtpServer.cs
@@ -2,7 +2,7 @@
 
 using System;
 using System.Collections;
-using System.IO;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -29,51 +29,7 @@ using System.Threading;
 
 namespace nDumbster.SmtpMockServer
 {
-	/// <summary>
-	/// Dummy SMTP server for testing purposes.
-	/// </summary>
-	/// <example>
-	/// The following examples shows how to use nDumbster and <see href="http://www.nunit.org">NUnit</see> to test your function sendMessage.
-	/// <code>
-	///	[TestFixture]
-	///	public class SimpleSmtpServerTest
-	///	{
-	///		SimpleSmtpServer smtpServer;
-	///
-	///		[SetUp]
-	///		public void Setup()
-	///		{
-	///
-	///			smtpServer = SimpleSmtpServer.Start();
-	///
-	///		}
-	///
-	///		[TearDown]
-	///		public void TearDown()
-	///		{
-	///			smtpServer.Stop();
-	///		}
-	///
-	///		[Test]
-	///		public void SendEmail()
-	///		{
-	///			/// Use you own code 
-	///			sendMessage(25, "sender@here.com", "Test", "Test Body", "receiver@there.com");
-	///			Assert.AreEqual(1, smtpServer.ReceivedEmailCount, "1 mails sent");
-	///			SmtpMessage mail= (SmtpMessage)smtpServer.ReceivedEmail[0];
-	///			Assert.AreEqual("&lt;receiver@there.com&gt;", mail.Headers["To"], "Receiver");
-	///			Assert.AreEqual("&lt;sender@here.com&gt;", mail.Headers["From"], "Sender");
-	///			Assert.AreEqual("Test", mail.Headers["Subject"], "Subject");
-	///
-	///			Assert.AreEqual("Test Body", mailUser.Body, "Body");
-	///		}
-	///	}
-	///	</code>
-	/// </example>
-	/// <threadsafety static="true" instance="true">
-	///	<para>The server create a thread to handle message reception and all access to messages list is protected.</para>
-	/// </threadsafety>
-	public class SimpleSmtpServer : IDisposable
+    public class SimpleSmtpServer : IDisposable
 	{
 
         public delegate void MessageReceivedHandler(MailMessageWrapper mmw);
@@ -94,11 +50,7 @@ namespace nDumbster.SmtpMockServer
 		/// </summary>
 		private int port = SimpleSmtpServer.DEFAULT_SMTP_PORT;
 		
-		/// <summary>
-		/// Stores all of the email received since this instance started up.
-		/// </summary>
-		private ArrayList receivedMail = new ArrayList();
-		
+	
 		/// <summary>
 		/// Indicates whether this server is stopped or not.
 		/// </summary>
@@ -158,47 +110,19 @@ namespace nDumbster.SmtpMockServer
 			}
 		}
 
-		/// <summary>
-		/// List of email received by this instance since start up.
-		/// </summary>
-		/// <value><see cref="Array">Array</see> holding received <see cref="SmtpMessage">SmtpMessage</see></value>
-		virtual public SmtpMessage[] ReceivedEmail
+        private static List<SmtpMessage> receivedEmail;
+		public static List<SmtpMessage> ReceivedEmail
 		{
-			get
-			{
-				lock(this)
-				{
-					return (SmtpMessage[])receivedMail.ToArray(typeof(SmtpMessage));
-				}
-			}
+            get
+            {
+                if (receivedEmail == null)
+                {
+                    receivedEmail = new List<SmtpMessage>();
+                }
+                return receivedEmail;
+            }
 		}
 
-		/// <summary>
-		/// Erase list of received emails
-		/// </summary>
-		virtual public void ClearReceivedEmail()
-		{
-			lock(this)
-			{
-				receivedMail.Clear();
-			}
-		}
-
-		/// <summary>
-		/// Number of messages received by this instance since start up.
-		/// </summary>
-		/// <value>Number of messages</value>
-		virtual public int ReceivedEmailCount
-		{
-			get
-			{
-				lock(this)
-				{
-					return receivedMail.Count;
-				}
-			}
-
-		}
 		#endregion  // Properties
 
 		/// <summary>
@@ -230,38 +154,31 @@ namespace nDumbster.SmtpMockServer
 					// or that something bad happened.
 					startedEvent.Set();
 				}
-
-				// Server: loop until stopped
-				while (!Stopped)
-				{
-					Socket socket = null;
-					try
-					{
-						// Accept an incomming client connection
-						socket = tcpListener.AcceptSocket();
-					}
-					catch
-					{
-						if (socket != null)
-						{
-							socket.Close();
-						}
-						continue; // Non-blocking socket timeout occurred: try accept() again
-					}
-
-					// Get the input and output streams
-					NetworkStream networkStream = new NetworkStream(socket);
-					StreamReader input = new StreamReader(networkStream);
-					StreamWriter output = new StreamWriter(networkStream);
-
-					lock(this)
-					{
-						// Fetch all incomming messages from client, and add them to the queue
-						receivedMail.AddRange(HandleSmtpTransaction(output, input));
-					}
-					// Close client connection, and wait for another one
-					socket.Close();
-				}
+                int numberOfClients = 0;
+                // Server: loop until stopped
+                while (!Stopped)
+                {
+                    Socket socket = null;
+                    try
+                    {
+                        // Accept an incomming client connection
+                        socket = tcpListener.AcceptSocket();
+                    }
+                    catch
+                    {
+                        if (socket != null)
+                        {
+                            socket.Close();
+                        }
+                        continue; // Non-blocking socket timeout occurred: try accept() again
+                    }
+                    this.TriggerClientConnected();
+                    SimpleSmtpClicent client = new SimpleSmtpClicent(this, socket);
+                    Thread myThread = new Thread(new ThreadStart(client.HandleSmtpTransaction));
+                    numberOfClients += 1;
+                    myThread.IsBackground = true;
+                    myThread.Start();
+                }
 			}
 			catch (Exception e)
 			{
@@ -282,81 +199,24 @@ namespace nDumbster.SmtpMockServer
 			}
 		}
 
-		/// <summary>
-		/// Handle an SMTP transaction, i.e. all activity between initial connect and QUIT command.
-		/// </summary>
-		/// <param name="output">output stream</param>
-		/// <param name="input">input stream</param>
-		/// <returns>List of received SmtpMessages</returns>
-		private IList HandleSmtpTransaction(StreamWriter output, StreamReader input)
-		{
-			// Initialize the state machine
-			SmtpState smtpState = SmtpState.CONNECT;
-			SmtpRequest smtpRequest = new SmtpRequest(SmtpActionType.CONNECT, String.Empty, smtpState);
+        public void TriggerMessageReceived(MailMessageWrapper mmw)
+        {
+            if (OnMessageReceived != null)
+            {
+                OnMessageReceived(mmw);
+            }
+        }
 
-			// Execute the connection request
-			SmtpResponse smtpResponse = smtpRequest.Execute();
+        public void TriggerClientConnected()
+        {
+            Console.WriteLine("socket open.");
+        }
 
-			// Send initial response
-			SendResponse(output, smtpResponse);
-			smtpState = smtpResponse.NextState;
+        public void TriggerClientDisconnected()
+        {
+            Console.WriteLine("socket close.");
+        }
 
-			IList msgList = new ArrayList();
-			SmtpMessage msg = new SmtpMessage();            
-			while (smtpState != SmtpState.CONNECT)
-			{
-				string line = input.ReadLine();
-
-				if (line == null)
-				{
-					break;
-				}
-
-				// Create request from client input and current state
-				SmtpRequest request = SmtpRequest.CreateRequest(line, smtpState);
-				// Execute request and create response object
-				SmtpResponse response = request.Execute();
-				// Move to next internal state
-				smtpState = response.NextState;
-				// Send reponse to client
-				SendResponse(output, response);
-
-				// Store input in message
-				msg.Store(response, request.Params);
-
-				// If message reception is complete save it
-				if (smtpState == SmtpState.QUIT)
-				{
-					msgList.Add(msg);
-				    if (OnMessageReceived != null)
-				    {
-                        MailMessageWrapper mmp = MailMessageWrapper.ConvertFrom(msg);
-                        OnMessageReceived(mmp);
-				    }
-				    msg = new SmtpMessage();
-				}
-			    if (request.GetAction()== SmtpActionType.EHLO)
-			    {
-			        msg.HostName = request.Params;
-			    }
-			}
-
-			return msgList;
-		}
-
-		/// <summary>
-		/// Send response to client.
-		/// </summary>
-		/// <param name="output">socket output stream</param>
-		/// <param name="smtpResponse">Response to send</param>
-		private void SendResponse(StreamWriter output, SmtpResponse smtpResponse)
-		{
-			if (smtpResponse.Code > 0)
-			{
-				output.WriteLine(smtpResponse.Code + " " + smtpResponse.Message);
-				output.Flush();
-			}
-		}
 
 		/// <summary>
 		/// Forces the server to stop after processing the current request.
